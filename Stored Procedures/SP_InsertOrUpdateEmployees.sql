c8b3815e974758c8c05493bcc0f31b6542e4ba3b
Disha Kamble <120121137+dishak24@users.noreply.github.com>
2025-03-19 16:01:33 +0530

    Given SP problem

--- Stored Procedures/SP_InsertOrUpdateEmployees.sql
+++ Stored Procedures/SP_InsertOrUpdateEmployees.sql
@@ -1,31 +1,31 @@
 select * from Employees;
 
---
-Alter procedure SP_InsertOrUpdateEmployees
-( @EmployeeID int, @Name varchar(25), @Age int, @Department varchar(25), @Salary money)
-as
-begin
-
-	if Exists( Select EmployeeID from Employees where EmployeeID = @EmployeeID )
+--Take id as a input and if id exist then update record otherwise insert new record
+	Create or Alter procedure SP_InsertOrUpdateEmployees
+	( @EmployeeID int, @Name varchar(25), @Age int, @Department varchar(25), @Salary money)
+	as
 	begin
-
-		update 	Employees
-		set Name = @Name, 
-		Salary = @Salary
-		where EmployeeID = @EmployeeID
-
-		print 'EmployeeID is already exist. So, Data Updated !'
-	end
-
-	else
+	
+		if Exists( Select EmployeeID from Employees where EmployeeID = @EmployeeID )
 		begin
-			Insert into Employees(EmployeeID, Name, Age, Department, Salary)
-			values (@EmployeeID, @Name, @Age, @Department, @Salary);
-
-			print 'EmployeeID is not exist. So, New Data Inserted !'
+	
+			update 	Employees
+			set Name = @Name, 
+			Salary = @Salary
+			where EmployeeID = @EmployeeID
+	
+			print 'EmployeeID is already exist. So, Data Updated !'
 		end
-
-end
+	
+		else
+			begin
+				Insert into Employees(EmployeeID, Name, Age, Department, Salary)
+				values (@EmployeeID, @Name, @Age, @Department, @Salary);
+	
+				print 'EmployeeID is not exist. So, New Data Inserted !'
+			end
+	
+	end
 
 
 --Excecute
