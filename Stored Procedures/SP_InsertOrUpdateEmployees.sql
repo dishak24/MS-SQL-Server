5fc425d23077133815e0fea9387f1cf8be23ce2e
Disha Kamble <120121137+dishak24@users.noreply.github.com>
2025-03-19 15:59:03 +0530

    Given task of Stored Procedure

---
+++ Stored Procedures/SP_InsertOrUpdateEmployees.sql
@@ -0,0 +1,37 @@
+select * from Employees;
+
+--
+Alter procedure SP_InsertOrUpdateEmployees
+( @EmployeeID int, @Name varchar(25), @Age int, @Department varchar(25), @Salary money)
+as
+begin
+
+	if Exists( Select EmployeeID from Employees where EmployeeID = @EmployeeID )
+	begin
+
+		update 	Employees
+		set Name = @Name, 
+		Salary = @Salary
+		where EmployeeID = @EmployeeID
+
+		print 'EmployeeID is already exist. So, Data Updated !'
+	end
+
+	else
+		begin
+			Insert into Employees(EmployeeID, Name, Age, Department, Salary)
+			values (@EmployeeID, @Name, @Age, @Department, @Salary);
+
+			print 'EmployeeID is not exist. So, New Data Inserted !'
+		end
+
+end
+
+
+--Excecute
+Exec SP_InsertOrUpdateEmployees
+@EmployeeID = 1, 
+@Name = 'Prachi',
+@Age = 24, 
+@Department = 'IT', 
+@Salary = 25000;
